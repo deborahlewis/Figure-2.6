68896929b1091c3e460a77d4de975809ee07b676
Deborah Lewis <deborahlewis@users.noreply.github.com>
2016-09-09 10:52:00 -0400

    Add files via upload

---
+++ Deitel_figure2_6.java
@@ -0,0 +1,16 @@
+/*
+ * Deborah Lewis
+ * Figure 2.6 Deitel: How to Program (Early Ojects)
+ * Displaying multiple lines with method System.out.printf
+ */
+package deitel_figure2_6;
+
+
+public class Deitel_figure2_6 {
+
+
+    public static void main(String[] args) {
+        System.out.printf("%s%n%s%n", "Welcome to", "Java Programming!");
+    } //end main
+    
+}
